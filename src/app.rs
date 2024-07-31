9d79d488976241f90106d49586082936b7ffc548
byron <byron@live.com>
2024-07-31 21:50:01 +0800

    增加对 app.rs 的注释

--- src/app.rs
+++ src/app.rs
@@ -6,38 +6,49 @@ use iced::{
 };
 use log::error;
 
+// App 消息,
+// Action 会传递给页面处理，Navigate 用于控制页面跳转
 #[derive(Debug, Clone)]
 pub enum Message {
-    LoadedFont(Result<(), iced::font::Error>),
-    Navigate(usize),
-    Action(pages::Page),
-    EventOccurred(Event),
+    LoadedFont(Result<(), iced::font::Error>), // 字体加载
+    Navigate(usize),                           // 页面导航
+    Action(pages::Page),                       // 传递给页面处理
+    EventOccurred(Event),                      // 事件处理(键盘等)
 }
 
+// App 状态机
 #[derive(Debug)]
 pub enum TinyApp {
+    // Loading 状态
     Loading,
+    // Loaded 状态
     Loaded {
-        idx: usize,
-        state: State,
-        pages: Vec<&'static pages::Page>,
+        idx: usize,                       // 当前所在页面
+        state: State,                     // 共享数据
+        pages: Vec<&'static pages::Page>, // 所有页面实例集合
     },
 }
 
+// 共享数据
 #[derive(Debug, Default)]
 pub struct State {
     pub font: Option<Font>,
 }
 
+// 字体加载(字体文件在编译时被包含进来)
 const FONT_BYTES_MSYH: &[u8] = include_bytes!("msyh.ttc");
+
+// 构建编译时可能用的信息
 include!(concat!(env!("OUT_DIR"), "/build_info.rs"));
 
+// 实现 TinyApp
 impl Application for TinyApp {
     type Executor = iced::executor::Default;
     type Message = Message;
     type Flags = ();
     type Theme = iced::theme::Theme;
 
+    // 初始化应用程序
     fn new(_flags: ()) -> (TinyApp, iced::Command<Self::Message>) {
         (
             TinyApp::Loading,
@@ -45,10 +56,12 @@ impl Application for TinyApp {
         )
     }
 
+    // 设置标题
     fn title(&self) -> String {
         format!("Tiny build: {}", BUILD_DATE)
     }
 
+    // 处理(状态与消息）
     fn update(&mut self, message: Message) -> iced::Command<Message> {
         match self {
             // TinyApp 状态机 （只处理字体加载）
@@ -104,6 +117,7 @@ impl Application for TinyApp {
         iced::Command::none()
     }
 
+    // 处理界面显示
     fn view(&self) -> iced::Element<Message> {
         match self {
             TinyApp::Loading => iced::Element::new(iced::widget::Text::new("Loading...")),
@@ -113,9 +127,13 @@ impl Application for TinyApp {
             }
         }
     }
+
+    // 控制主题
     fn theme(&self) -> Self::Theme {
         iced::theme::Theme::Dark
     }
+
+    // 订阅事件
     fn subscription(&self) -> iced::Subscription<Self::Message> {
         iced::Subscription::batch(vec![event::listen().map(Message::EventOccurred)])
     }
