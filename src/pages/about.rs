2e9300e98bcfe8daf4763c26c87677bb06da0222
byron <byron@live.com>
2024-08-01 21:30:14 +0800

    生命周期注解

--- src/pages/about.rs
+++ src/pages/about.rs
@@ -9,6 +9,8 @@ pub enum SubMessage {
 
 impl super::PageAction<SubMessage> for SubMessage {
     fn update(&self, idx: &mut usize, state: &mut super::State) -> iced::Command<SubMessage> {
+        // 如果编译过程中发现 update::proccess 同样会有数据涉及生命周期的问题，可能还需要加上生命周期的注解
+        // 本例中没有进行任何操作
         return update::proccess(idx, state);
     }
 
