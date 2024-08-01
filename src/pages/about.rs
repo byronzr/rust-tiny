40962580e9508bbf0a91ee7eb446877b39b0d3ba
byron <byron@live.com>
2024-08-01 21:22:38 +0800

    增加标注生命周期注解的原因

--- src/pages/about.rs
+++ src/pages/about.rs
@@ -13,6 +13,9 @@ impl super::PageAction<SubMessage> for SubMessage {
     }
 
     fn view<'a>(&self, state: &'a super::State) -> iced::Element<'a, SubMessage> {
+        // 将 state 传递给 view::show 函数
+        // view::show 的返回，会涉及到生命周期的，所以需要作生命周期的注解
+        // 而生命周期的注解涉及到 PageAction Trait,还有 proc_macro 的实现
         view::show(state)
     }
 }
